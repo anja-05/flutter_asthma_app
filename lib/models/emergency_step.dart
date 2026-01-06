297129698324067d78fc89ac380429854f602f8a
lisap <poehllisa@gmail.com>
2026-01-06 20:29:34 +0100

    dartdoc for emergency_step.dart

--- lib/models/emergency_step.dart
+++ lib/models/emergency_step.dart
@@ -1,4 +1,6 @@
-/// Datenmodell für einen einzelnen Notfall-Schritt.
+/// Repräsentiert einen einzelnen Notfall-Schritt.
+///
+/// Jede Instanz beschreibt eine Aufgabe, die abgearbeitet werden soll, und speichert, ob sie bereits erledigt wurde.
 class EmergencyStep {
   final String text;
   bool completed;
